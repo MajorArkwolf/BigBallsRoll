fcf03002a1bf2b4adae294dc2bb854b05a964994
Chase-Percy <chasepercy@yahoo.co.nz>
2020-12-31 14:37:58 +0800

    Fixed doxygen comments in textureManager.h

--- src/Engine/textureManager.h
+++ src/Engine/textureManager.h
@@ -22,8 +22,9 @@ typedef struct TextureManager {
 } TextureManager;
 
 /**
- * Frees the textureData and sets textureName to NULL
- * @param texture Texture struct to store Name and Data
+ * Frees textureData and textureName
+ * Sets Width, Height, Channels to zero
+ * @param texture Texture struct to store Name, Data, Width, Height, Channels
  */
 void Texture_free(Texture *texture);
 
@@ -41,7 +42,7 @@ void TextureManager_init(TextureManager *textureManager);
 
 /**
  * Free objects contained inside of the texture manager
- * @param textureManager Texture manager to be freed
+ * @param textureManager Texture manager with the objects to be freed
  */
 void TextureManager_free(TextureManager *textureManager);
 
@@ -62,19 +63,20 @@ void TextureManager_preLoadTextures(TextureManager *textureManager, char *cwd);
 bool TextureManager_loadTexture(TextureManager *textureManager, char *cwd, char *textureName);
 
 /**
- * Finds a texture based on a name and returns its ID (index) in the texture managers textures array
- * If texture cannot be found, 0 (default texture) is returned
+ * Finds a texture based on a name and returns the Texture object
+ * If texture cannot be found the texture at 0 (DEFAULT_TEXTURE) is returned
  * @param textureManager The texture manager to be searched
+ * @param cwd The current working directory, must contain res folder to work
  * @param textureName The name of the texture to search for
  * @return the index of the texture in the array, if not there 0 (default texture) is returned
  */
 Texture* TextureManager_getTexture(TextureManager *textureManager, char *cwd, char *textureName);
 
 /**
- * Gets a texture object if it is found in the texture manager
+ * Gets a texture object if it is found in the texture manager using the index provided
  * @param textureManager The texture manager to search
- * @param index The index of the texture
- * @return The texture object if found, else DEFAULT_TEXTURE (0)
+ * @param index The index of the texture to be returned
+ * @return The texture object if found, else DEFAULT_TEXTURE (index 0)
  */
 Texture* TextureManager_getTextureUsingID(TextureManager *textureManager, size_t index);
 
@@ -82,7 +84,7 @@ Texture* TextureManager_getTextureUsingID(TextureManager *textureManager, size_t
  * Finds a textures ID (index) in the textureManager using the texture name
  * @param textureManager The texture manager to search
  * @param textureName The name of the texture to search for
- * @return The ID (index) of the texture or DEFAULT_TEXTURE (0) if not found
+ * @return The ID (index) of the texture or DEFAULT_TEXTURE (index 0) if not found
  */
 size_t TextureManager_findTextureID(TextureManager *textureManager, char *textureName);
 

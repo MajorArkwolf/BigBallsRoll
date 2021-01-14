5c0459572165b77cc46980d09c924b2d65f04fb5
Matthew Davis <matt@mhwdvs.com>
2021-01-14 15:18:21 +0800

    Create initial physics engine functions and additional files

--- lib/Physics/src/include/BigBalls/physicsEngine.h
+++ lib/Physics/src/include/BigBalls/physicsEngine.h
@@ -1,2 +1,42 @@
 #pragma once
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * Initialises the physics world
+ */
+void physicsWorldInit();
+
+/**
+ * Creates a collision body (a collection of colliders dedicated to detecting collisions of an object)
+ * @return int ID of the collision body created
+ */
+size_t createCollisionBody();
+
+/**
+ * Destroys a collision body
+ * @param ID the ID of the collision body to be destroyed
+ * @return bool Success of operation
+ */
+bool destroyCollisionBody(size_t ID);
+
+/**
+ * Creates a box collider in a collision body
+ */
+void createBoxCollider(size_t ID,
+                       size_t xOffset,
+                       size_t yOffset,
+                       size_t zOffset,
+                       size_t xLen,
+                       size_t yLen,
+                       size_t zLen);
+
+/**
+ * Creates a sphere collider in a collision body
+ */
+void createSphereCollider(size_t ID,
+                          size_t xOffset,
+                          size_t yOffset,
+                          size_t zOffset,
+                          size_t radius);
 
--- lib/Physics/src/physicsEngine.c
+++ lib/Physics/src/physicsEngine.c
@@ -1,2 +1,48 @@
 #include "include/BigBalls/physicsEngine.h"
+#include "include/BigBalls/collisionBodyManager.h"
+#include <stdbool.h>
+#include <stdlib.h>
 
+void collisionBodyManager(){
+    // init collider manager
+}
+
+void physicsWorldInit(){
+    // init collision body manager
+}
+
+size_t createCollisionBody(){
+    // Allocate memory for collisionbody
+    // Add memory to collision body manager and get ID from manager
+    return 0; // stub
+}
+
+bool destroyCollisionBody(size_t ID){
+    return true; // stub
+}
+
+void createBoxCollider(size_t ID,
+                       size_t xOffset,
+                       size_t yOffset,
+                       size_t zOffset,
+                       size_t xLen,
+                       size_t yLen,
+                       size_t zLen){
+
+}
+
+void destroyBoxCollider(){
+
+}
+
+void createSphereCollider(size_t ID,
+                          size_t xOffset,
+                          size_t yOffset,
+                          size_t zOffset,
+                          size_t radius){
+
+}
+
+void destroySphereCollider(){
+
+}

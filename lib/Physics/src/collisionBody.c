9b67ebe1993df1ce095826393659346fb67d961b
Matthew Davis <matt@mhwdvs.com>
2021-01-18 00:50:56 +0800

    Implement [very basic] CollisionBody

--- lib/Physics/src/collisionBody.c
+++ lib/Physics/src/collisionBody.c
@@ -1,1 +1,11 @@
+#include "include/BigBalls/collisionBody.h"
+#include <assert.h>
 
+void CollisionBody_init(CollisionBody *collisionBody){
+    collisionBody->colliderManager = calloc(1, sizeof(ColliderManager));
+}
+
+void CollisionBody_free(CollisionBody *collisionBody){
+    assert(collisionBody != NULL);
+    ColliderManager_free(collisionBody->colliderManager);
+}
--- lib/Physics/src/include/BigBalls/collisionBody.h
+++ lib/Physics/src/include/BigBalls/collisionBody.h
@@ -1,4 +1,19 @@
 #pragma once
 
+#include "colliderManager.h"
+
 typedef struct CollisionBody{
+    ColliderManager* colliderManager;
 } CollisionBody;
+
+/**
+ * Initialise collisionBody
+ * @param collisionBody
+ */
+void CollisionBody_init(CollisionBody *collisionBody);
+
+/**
+ * Free collisionBody
+ * @param collisionBody
+ */
+void CollisionBody_free(CollisionBody *collisionBody);

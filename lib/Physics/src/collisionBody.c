27516e09ea38ca9370b1479c30e48ce242b46606
Matthew Davis <matt@mhwdvs.com>
2021-01-18 13:57:26 +0800

    Remove redundant colliderManager (too much overlap with role of collisionBody)

--- lib/Physics/src/collisionBody.c
+++ lib/Physics/src/collisionBody.c
@@ -1,11 +1,115 @@
 #include "include/BigBalls/collisionBody.h"
 #include <assert.h>
 
+int getID(CollisionBody *collisionBody){
+    assert(collisionBody != NULL);
+    // increment and return new id
+    return collisionBody->idCount++;
+}
+
 void CollisionBody_init(CollisionBody *collisionBody){
-    collisionBody->colliderManager = calloc(1, sizeof(ColliderManager));
+    assert(collisionBody != NULL);
+    collisionBody->numOfColliders = 0;
+    collisionBody->numOfBoxColliders = 0;
+    collisionBody->numOfSphereColliders = 0;
+    collisionBody->BoxColliders = NULL;
+    collisionBody->SphereColliders = NULL;
+    collisionBody->idCount = 0;
+    collisionBody->id = -1; //TODO: request id here or not?
 }
 
 void CollisionBody_free(CollisionBody *collisionBody){
     assert(collisionBody != NULL);
-    ColliderManager_free(collisionBody->colliderManager);
+    // Free stack-allocated box colliders
+    for(size_t i = 0; i < collisionBody->numOfBoxColliders; ++i){
+        free(collisionBody->BoxColliders[i]);
+    }
+    // Free stack-allocated sphere colliders
+    for(size_t i = 0; i < collisionBody->numOfSphereColliders; ++i){
+        free(collisionBody->SphereColliders[i]);
+    }
+    // Reset colliderManager values
+    collisionBody->numOfBoxColliders = 0;
+    collisionBody->numOfSphereColliders = 0;
+    collisionBody->numOfColliders = 0;
+}
+
+void CollisionBody_addBoxCollider(CollisionBody *collisionBody,
+                                  BoxCollider *boxCollider){
+    // Allocate new, larger array
+    if(collisionBody->BoxColliders == NULL){
+        collisionBody->BoxColliders = calloc(1, sizeof(BoxCollider));
+    }
+    else{
+        collisionBody->BoxColliders = realloc(collisionBody->BoxColliders, sizeof(BoxCollider) * collisionBody->numOfBoxColliders + 1);
+    }
+    // Copy BoxCollider object into array
+    collisionBody->BoxColliders[collisionBody->numOfBoxColliders] = boxCollider;
+    // Assign ID to BoxCollider
+    collisionBody->BoxColliders[collisionBody->numOfBoxColliders]->id = getID(collisionBody);
+    collisionBody->numOfBoxColliders++;
+    collisionBody->numOfColliders++;
+}
+
+void CollisionBody_rmBoxCollider(CollisionBody *collisionBody,
+                                   int id){
+    if(collisionBody->numOfBoxColliders - 1 != 0){
+        // Allocate memory for smaller array
+        BoxCollider **newBoxColliders = calloc(collisionBody->numOfBoxColliders - 1, sizeof(BoxCollider));
+        // Only copy elements that are not to be removed
+        for(size_t i = 0; i < collisionBody->numOfBoxColliders - 1; ++i){
+            if(collisionBody->BoxColliders[i]->id != id){
+                newBoxColliders[i] = collisionBody->BoxColliders[i];
+            }
+        }
+        // free old array
+        free(collisionBody->BoxColliders);
+        // assign new array to colliderManager
+        collisionBody->BoxColliders = newBoxColliders;
+    }
+    else{
+        // if size = 0, store null pointer
+        free(collisionBody->BoxColliders);
+        collisionBody->BoxColliders = NULL;
+    }
+}
+
+void CollisionBody_addSphereCollider(CollisionBody *collisionBody,
+                                       SphereCollider *sphereCollider){
+    // Allocate new, larger array
+    if(collisionBody->SphereColliders == NULL){
+        collisionBody->SphereColliders = calloc(1, sizeof(SphereCollider));
+    }
+    else{
+        collisionBody->SphereColliders = realloc(collisionBody->SphereColliders, sizeof(SphereCollider) * collisionBody->numOfSphereColliders + 1);
+    }
+    // Copy SphereCollider object into array
+    collisionBody->SphereColliders[collisionBody->numOfSphereColliders] = sphereCollider;
+    // Assign ID to BoxCollider
+    collisionBody->SphereColliders[collisionBody->numOfSphereColliders]->id = getID(collisionBody);
+    collisionBody->numOfSphereColliders++;
+    collisionBody->numOfColliders++;
+}
+
+void CollisionBody_rmSphereCollider(CollisionBody *collisionBody,
+                                      int id){
+    if(collisionBody->numOfSphereColliders - 1 != 0){
+        // Allocate memory for smaller array
+        SphereCollider **newSphereColliders = calloc(collisionBody->numOfSphereColliders - 1, sizeof(SphereCollider));
+        // Only copy elements that are not to be removed
+        for(size_t i = 0; i < collisionBody->numOfSphereColliders - 1; ++i){
+            if(collisionBody->SphereColliders[i]->id != id){
+                newSphereColliders[i] = collisionBody->SphereColliders[i];
+            }
+        }
+        // free old array
+        free(collisionBody->SphereColliders);
+        // assign new array to colliderManager
+        collisionBody->SphereColliders = newSphereColliders;
+    }
+    else{
+        // if size = 0, store null pointer
+        free(collisionBody->SphereColliders);
+        collisionBody->SphereColliders = NULL;
+    }
 }
--- lib/Physics/src/include/BigBalls/collisionBody.h
+++ lib/Physics/src/include/BigBalls/collisionBody.h
@@ -1,9 +1,15 @@
 #pragma once
 
-#include "colliderManager.h"
+#include "collisionBodyManager.h"
 
 typedef struct CollisionBody{
-    ColliderManager* colliderManager;
+    // implementation of stack
+    BoxCollider **BoxColliders;
+    size_t numOfBoxColliders;
+    SphereCollider **SphereColliders;
+    size_t numOfSphereColliders;
+    size_t numOfColliders; // TODO: may be redundant
+    int idCount;
     int id;
 } CollisionBody;
 
@@ -18,3 +24,20 @@ void CollisionBody_init(CollisionBody *collisionBody);
  * @param collisionBody
  */
 void CollisionBody_free(CollisionBody *collisionBody);
+
+/**
+ * A stack array was chosen for its ability to be resized, at the cost of performance when being resized (when colliders are added or destroyed). As object creation/destruction are not speed-critical operations in our project this tradeoff seems acceptable (the alternative being two static arrays per collisonBody (for each type) which seemed a bit excessive)
+ * @param colliderManager
+ * @param boxCollider
+ */
+void CollisionBody_addBoxCollider(CollisionBody *collisionBody,
+                                    BoxCollider *boxCollider);
+
+void CollisionBody_rmBoxCollider(CollisionBody *collisionBody,
+                                   int id);
+
+void CollisionBody_addSphereCollider(CollisionBody *collisionBody,
+                                       SphereCollider *sphereCollider);
+
+void CollisionBody_rmSphereCollider(CollisionBody *collisionBody,
+                                      int id);

fca4f18914f05898e7e2941a6d27f3ac3a3026de
Matthew Davis <matt@mhwdvs.com>
2021-01-27 13:38:52 +0800

    Fix doxygen on CollisionBodyManager

--- lib/Physics/src/include/BigBalls/collisionBodyManager.h
+++ lib/Physics/src/include/BigBalls/collisionBodyManager.h
@@ -8,12 +8,30 @@ typedef struct CollisionBodyManager{
     int idCount;
 } CollisionBodyManager;
 
+/**
+ * Initialises a CollisionBodyManager object
+ * @param collisionBodyManager The CollisionBodyManager to be initialised
+ */
 void CollisionBodyManager_init(CollisionBodyManager *collisionBodyManager);
 
+/**
+ * Frees a CollisionBodyManager from memory
+ * @param collisionBodyManager The CollisionBodyManager to be freed
+ */
 void CollisionBodyManager_free(CollisionBodyManager *collisionBodyManager);
 
+/**
+ * Adds a CollisionBody to a CollisionBodyManager
+ * @param collisionBodyManager The target CollisionBodyManager
+ * @param collisionBody The CollisionBOdy to be added
+ */
 void CollisionBodyManager_add(CollisionBodyManager *collisionBodyManager,
                               CollisionBody *collisionBody);
 
+/**
+ * Removes a CollisionBody from a CollisionBodyManager via its ID
+ * @param collisionBodyManager The target CollisionBodyManager
+ * @param id The ID of the CollisionBody to be removed
+ */
 void CollisionBodyManager_rm(CollisionBodyManager *collisionBodyManager,
                              int id);

bf917e49840d4e99bc588e80b19d609863693001
Matthew Davis <matt@mhwdvs.com>
2021-01-27 13:09:47 +0800

    Fix doxygen param comments

--- lib/Physics/src/include/BigBalls/collisionBody.h
+++ lib/Physics/src/include/BigBalls/collisionBody.h
@@ -23,13 +23,13 @@ typedef struct CollisionBody{
 
 /**
  * Initialise collisionBody
- * @param collisionBody
+ * @param collisionBody The CollisionBody object to initialise
  */
 void CollisionBody_init(CollisionBody *collisionBody);
 
 /**
  * Free collisionBody
- * @param collisionBody
+ * @param collisionBody The CollisionBody object to free
  */
 void CollisionBody_free(CollisionBody *collisionBody);
 
@@ -37,42 +37,42 @@ void CollisionBody_free(CollisionBody *collisionBody);
  * A heap array was chosen for its ability to be resized, at the cost of performance when being resized (when colliders are added or destroyed).
  * As object creation/destruction are not speed-critical operations in our project this tradeoff seems acceptable
  * (the alternative being two static arrays per collisonBody (for each type) which seemed a bit excessive)
- * @param colliderManager
- * @param boxCollider
+ * @param collisionBody The CollisionBody object to add a BoxCollider object to
+ * @param boxCollider The BoxCollider object to be added to the CollisionBody
  */
 void CollisionBody_addBoxCollider(CollisionBody *collisionBody,
                                     BoxCollider *boxCollider);
 
 /**
  * Remove a boxcollider from a collisionbody
- * @param collisionBody
- * @param id
+ * @param collisionBody The CollisionBody to remove a BoxCollider from
+ * @param id The ID of a BoxCollider to be removed from the CollisionBody
  */
 void CollisionBody_rmBoxCollider(CollisionBody *collisionBody,
                                    int id);
 
 /**
  * Add a spherecollider to a collisionbody
- * @param collisionBody
- * @param sphereCollider
+ * @param collisionBody The CollisionBody object to add a SphereCollider object to
+ * @param sphereCollider The SphereCollider object to be added to the CollisionBody
  */
 void CollisionBody_addSphereCollider(CollisionBody *collisionBody,
                                        SphereCollider *sphereCollider);
 
 /**
  * Remove a spherecollider from a collisionbody (by ID)
- * @param collisionBody
- * @param id
+ * @param collisionBody The CollisionBody to remove a SphereCollider from
+ * @param id The ID of a SphereCollider to be removed from the CollisionBody
  */
 void CollisionBody_rmSphereCollider(CollisionBody *collisionBody,
                                       int id);
 
 /**
  * Translate a collisonbody (and therefore all of its member colliders)
- * @param collisionBody
- * @param xDist
- * @param yDist
- * @param zDist
+ * @param collisionBody The CollisionBody to be translated
+ * @param xDist The x-delta the CollisionBody should be moved by
+ * @param yDist The y-delta the CollisionBody should be moved by
+ * @param zDist The z-delta the CollisionBody should be moved by
  */
 void CollisionBody_translate(CollisionBody *collisionBody,
                              float xDist,
@@ -81,10 +81,10 @@ void CollisionBody_translate(CollisionBody *collisionBody,
 
 /**
  * Rotate a collisionbody (and therefore all of its member colliders)
- * @param collisionBody
- * @param xRot
- * @param yRot
- * @param zRot
+ * @param collisionBody The CollisionBody to be rotated
+ * @param xRot The x-delta the CollisionBody should be rotated by
+ * @param yRot The y-delta the CollisionBody should be rotated by
+ * @param zRot The z-delta the CollisionBody should be rotated by
  */
 void CollisionBody_rotate(CollisionBody *collisionBody,
                           float xRot,
@@ -93,7 +93,8 @@ void CollisionBody_rotate(CollisionBody *collisionBody,
 
 /**
  * Scale a collisionbody uniformly (and therefore all of its member colliders)
- * @param collisionBody
- * @param scaleFactor
+ * @param collisionBody The CollisionBody to be uniformly scaled
+ * @param scaleFactor The scale factor the CollisionBody should be scaled by
+ * (scale factor of 1 would have no change, 0.5 would be half size, 2 would be double size)
  */
 void CollisionBody_scale(CollisionBody *collisionBody, float scaleFactor);

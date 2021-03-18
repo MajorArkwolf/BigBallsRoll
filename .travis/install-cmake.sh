8f1eceb6175f98ac0d393bcb750bf01c3b5b063b
Peter <petercrabbe92@gmail.com>
2021-03-18 23:20:24 +0800

    attempt to fix windows errors

--- .travis/install-cmake.sh
+++ .travis/install-cmake.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ "${TRAVIS_OS_NAME}" != "windows" ]]; then
-set -x
-tmp=$(mktemp -d)
-cd tmp
-wget https://cmake.org/files/v3.17/cmake-3.17.0-rc1-Linux-x86_64.tar.gz
-tar -xf cmake-3.17.0-rc1-Linux-x86_64.tar.gz
-cd cmake-3.17.0-rc1-Linux-x86_64
-sudo cp -rf {bin,share} /usr/local/
+  set -x
+  tmp=$(mktemp -d)
+  cd tmp
+  wget https://cmake.org/files/v3.17/cmake-3.17.0-rc1-Linux-x86_64.tar.gz
+  tar -xf cmake-3.17.0-rc1-Linux-x86_64.tar.gz
+  cd cmake-3.17.0-rc1-Linux-x86_64
+  sudo cp -rf {bin,share} /usr/local/
 fi

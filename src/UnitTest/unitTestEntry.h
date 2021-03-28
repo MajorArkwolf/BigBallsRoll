3323f41d89f6f45efa4b2871e8cb41840b31cdd6
Peter <petercrabbe92@gmail.com>
2021-03-28 14:01:35 +0800

    setup ready for testing framework

--- src/UnitTest/unitTestEntry.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <stdbool.h>
-
-//bool beginUnitTests(int argc, char* argv[]) { return true; }
--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include <string.h>
 #include <stdio.h>
 #include "Engine/engine.h"
-#include "UnitTest/unitTestEntry.h"
 
 int main(int argc, char *argv[]) {
     if (argc > 1 && strcmp(argv[1], "--test") == 0) {

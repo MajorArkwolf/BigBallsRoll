d1fcd9a36084449b8e80a17c5cb1285ca079cae9
Chase <60807752+Chase-Percy@users.noreply.github.com>
2021-04-14 12:20:51 +0800

    update guiManager

--- src/Engine/guiManager.c
+++ src/Engine/guiManager.c
@@ -37,11 +37,6 @@ void guiManager_mainMenu() {
     /* GUI */
     if (nk_begin(GuiContainer.ctx, "Big Balls Roll!", nk_rect(50, 50, engine.width/4, engine.height/2),
                  NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|NK_WINDOW_TITLE)) {
-        static int seed = 0;
-        static int volume = 85;
-        static bool sound = true;
-        static int vertSens = 5;
-        static int horSens = 5;
 
         //Menu title
         nk_layout_row_dynamic(GuiContainer.ctx, engine.height / 32, 1);
@@ -52,7 +47,7 @@ void guiManager_mainMenu() {
         //Level settings
         if (nk_group_begin(GuiContainer.ctx, "Level",  NK_WINDOW_BORDER | NK_WINDOW_TITLE)) {
             nk_layout_row_dynamic(GuiContainer.ctx, engine.height / 32, 3);
-            nk_property_int(GuiContainer.ctx, "Seed:", INT_MIN, &seed, INT_MAX, 10, 10);
+            nk_property_int(GuiContainer.ctx, "Seed:", INT_MIN, &engine.playerConfig.seed, INT_MAX, 10, 10);
 
             nk_group_end(GuiContainer.ctx);
         }
@@ -62,11 +57,11 @@ void guiManager_mainMenu() {
         //Audio settings
         if (nk_group_begin(GuiContainer.ctx, "Audio", NK_WINDOW_BORDER | NK_WINDOW_TITLE)) {
             nk_layout_row_dynamic(GuiContainer.ctx, engine.height / 32, 3);
-            nk_property_int(GuiContainer.ctx, "Volume:", 0, &volume, 100, 10, 10);
+            nk_property_int(GuiContainer.ctx, "Volume:", 0, &engine.playerConfig.volume, 100, 10, 10);
             nk_layout_row_dynamic(GuiContainer.ctx, engine.height / 32, 3);
             nk_label(GuiContainer.ctx, "Sound: ", NK_TEXT_LEFT);
-            if (nk_option_label(GuiContainer.ctx, "Enabled", sound == true)) sound = true;
-            if (nk_option_label(GuiContainer.ctx, "Disabled", sound == false)) sound = false;
+            if (nk_option_label(GuiContainer.ctx, "Enabled", engine.playerConfig.sound == true)) engine.playerConfig.sound = true;
+            if (nk_option_label(GuiContainer.ctx, "Disabled", engine.playerConfig.sound == false)) engine.playerConfig.sound = false;
             nk_group_end(GuiContainer.ctx);
         }
 
@@ -77,9 +72,9 @@ void guiManager_mainMenu() {
             nk_layout_row_dynamic(GuiContainer.ctx, engine.height / 32, 3);
             nk_label(GuiContainer.ctx, "Sensitivity: ", NK_TEXT_LEFT);
             nk_layout_row_dynamic(GuiContainer.ctx, engine.height / 32, 3);
-            nk_property_int(GuiContainer.ctx, "Vertical:", 0, &vertSens, 100, 10, 10);
+            nk_property_int(GuiContainer.ctx, "Vertical:", 0, &engine.playerConfig.verticalSens, 100, 10, 10);
             nk_layout_row_dynamic(GuiContainer.ctx, engine.height / 32, 3);
-            nk_property_int(GuiContainer.ctx, "Horizontal:", 0, &horSens, 100, 10, 10);
+            nk_property_int(GuiContainer.ctx, "Horizontal:", 0, &engine.playerConfig.horizontalSens, 100, 10, 10);
             nk_group_end(GuiContainer.ctx);
         }
 
@@ -87,10 +82,11 @@ void guiManager_mainMenu() {
         nk_layout_row_dynamic(GuiContainer.ctx, engine.height / 32, 1);
         if (nk_button_label(GuiContainer.ctx, "Confirm")) {
             //TODO:: pass the stuff
-            printf("Seed: %i\n", seed);
-            printf("sound: %i\n", sound);
-            printf("V sens: %i\n", vertSens);
-            printf("H sens: %i\n", horSens);
+            printf("Seed: %i\n", engine.playerConfig.seed);
+            printf("Volume: %i\n", engine.playerConfig.volume);
+            printf("sound: %i\n", engine.playerConfig.sound);
+            printf("V sens: %i\n", engine.playerConfig.verticalSens);
+            printf("H sens: %i\n", engine.playerConfig.horizontalSens);
         }
     }
     nk_end(GuiContainer.ctx);

0d893261a777d7e1badd8179df6d89e88f74e59f
Chase <60807752+Chase-Percy@users.noreply.github.com>
2021-04-26 10:01:22 +0800

    Add Doxygen

--- src/Engine/skybox.h
+++ src/Engine/skybox.h
@@ -1,12 +1,21 @@
 #pragma  once
 #include "Engine/textureManager.h"
 
+//TODO: Temp work around for windows
 #ifndef GL_CLAMP_TO_EDGE
 #define GL_CLAMP_TO_EDGE 0x812F
 #endif
 
+//Skybox
 Texture *skybox[6];
 
+/**
+ * Loads the 6 skybox textures into the skybox global.
+ * If the skybox cant be found the default texture will be used.
+ */
 void Skybox_loadTexture(void);
 
+/**
+ * Draws the skybox. Must be drawn before anything else is drawn.
+ */
 void Skybox_draw(void);

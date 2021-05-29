d29457459e5bc84f837e4273e9b14530fd4e3e12
Chase <60807752+Chase-Percy@users.noreply.github.com>
2021-05-29 20:04:30 +0800

    Check player isn't under node

--- res/Script/EndNode.lua
+++ res/Script/EndNode.lua
@@ -30,11 +30,13 @@ function EndNode:DestroyRandomBlock()
 end
 
 function EndNode:CheckEndTrigger(player)
-    if (self.position.x < player.position.x + self.width and self.position.x + 1 > player.position.x) then
-        if (self.position.z < player.position.z + self.width and self.position.z + 1 > player.position.z) then
-            return true
+    if (self.position.y <= player.position.y) then  -- Check player isn't under node
+        if (self.position.x < player.position.x + self.width and self.position.x + 1 > player.position.x) then
+            if (self.position.z < player.position.z + self.width and self.position.z + 1 > player.position.z) then
+                return true
+            end
         end
-    end
+     end
     return false
 end
 

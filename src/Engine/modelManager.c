3d636292c0822847350d4d01205bd081528da8b8
Chase <60807752+Chase-Percy@users.noreply.github.com>
2021-05-29 18:52:34 +0800

    Fix getModel Logic

--- src/Engine/modelManager.c
+++ src/Engine/modelManager.c
@@ -56,7 +56,7 @@ size_t ModelManager_findModel(ModelManager *modelManger, const char *modelName) 
 
 Model* ModelManager_getModel(ModelManager *modelManger, size_t index) {
     assert(modelManger != NULL);
-    if (index > modelManger->NumOfModels) {
+    if (index >= modelManger->NumOfModels) {
         printf("Index greater then number of models, return 0 index");
         assert(false);
         return &modelManger->Models[0];
